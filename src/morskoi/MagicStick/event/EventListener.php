ac540fc3ea62aa84e261dea91bc02e6a1053db97
morskoi den <deniscernysenko228@gmail.com>
2026-02-10 13:34:09 +0200

    Implement sound effect for Magic Stick usage
    Added sound effect when using the Magic Stick.

--- src/morskoi/MagicStick/event/EventListener.php
+++ src/morskoi/MagicStick/event/EventListener.php
@@ -5,12 +5,14 @@ namespace morskoi\MagicStick\event;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\item\ItemTypeIds;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 use pocketmine\world\particle\DustParticle;
 use pocketmine\color\Color;
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\VanillaEffects;
 use pocketmine\entity\Living;
 use morskoi\MagicStick\MagicStick;
+use pocketmine\network\mcpe\protocol\types\LevelSoundEvent;
 
 class EventListener implements Listener
 {
@@ -43,6 +45,16 @@ class EventListener implements Listener
 
             $color = new Color(255, 127, 0);
             $pos = $p->getPosition();
+            $pk = LevelSoundEventPacket::create(
+                LevelSoundEvent::EXPLODE,
+                $pos,
+                -1,
+                ":",
+                false,
+                false,
+                false,
+            );
+            $p->getNetworkSession()->sendDataPacket($pk);
             $world = $p->getWorld();
             $rad = 5;
             foreach($world->getNearbyEntities($p->getBoundingBox()->expandedCopy($rad, $rad, $rad)) as $entity) 
